28429348cb8fa82b6272a6f608745207b2dba8ad
sakshi-1604 <65847879+sakshi-1604@users.noreply.github.com>
2022-12-04 07:25:32 +0530

    First commit

---
+++ scripts/01_deploy.js
@@ -0,0 +1,30 @@
+async function main() {
+    [signer1, signer2, signer3] = await ethers.getSigners();
+  
+    const Donation = await ethers.getContractFactory('Donation', signer1);
+    const donation = await Donation.deploy();
+  
+    console.log("Donation contract deployed to:", donation.address, "by", signer1.address);
+  
+    await signer1.sendTransaction({
+      to: donation.address,
+      value: ethers.utils.parseUnits('0.1', 18)
+    });
+    await signer2.sendTransaction({
+      to: donation.address,
+      value: ethers.utils.parseUnits('0.1', 18)
+    });
+    await signer3.sendTransaction({
+      to: donation.address,
+      value: ethers.utils.parseUnits('2', 18)
+    });
+  }
+  
+  // npx hardhat run --network localhost scripts/01_deploy.js
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
